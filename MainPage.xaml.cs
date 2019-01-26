f4825228300f1a34fa754754f7681e92a989af88
Maximus <42292465+grushister@users.noreply.github.com>
2019-01-27 00:51:32 +0200

    Project
    Added main project's files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Telegram.Bot;
+
+
+namespace EasyTelegramBeta
+{
+    public partial class MainPage : ContentPage
+    {
+        static TelegramBotClient Bot;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Bot = new TelegramBotClient(EntryOne.Text);
+            Bot.SendTextMessageAsync(EntryTwo.Text, EntryThree.Text);
+        }
+
+    }
+}
